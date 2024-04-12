eb5ac2e4a7eeec91540e5f27efe84516926f1dc7
GeorgeKahugu <gee.kahugu@gmail.com>
2024-04-12 09:07:22 +0300

    added the function to generate random quote added the fetch link

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
 document.addEventListener("DOMContentLoaded",function(){
-    const quoteElement=document.getElementById("quotes")
-    // function to fetch from the API
+    const quoteElement=document.getElementById("quote");
+    const generateQuotesButton=document.getElementById("generateQuotes");
 
+    // Add Eventlistner for Generating Random Quotes
+generateQuotesButton.addEventListener("click",generateQuotes);
+
+    // function to fetch from the API
+function generateRandomQuote(){
+    fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    .then(Response => Response.json())
+    .then(data => {
+        if(data.length>0){
+            quoteElement.textContent= data[10];
+        }
+    }) 
+        .catch(error=>{
+            // console.error('Error fetching quote:', error);
+            
+        });
+}
+generateRandomQuote();
 
 });
 
